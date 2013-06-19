f30aebdc7018867bdfcd28908467844de674ad5a
Thorsten Ball <mrnugget@gmail.com>
2013-06-19 23:10:10 +0200

    Add dist.sh for building and packaging releases

---
+++ dist.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+# Builds and packages the binaries for Darwin/amd64 and Linux/amd64
+
+version=$(cat watchgopher.go | grep "const VERSION" | awk '{print $NF}' | sed 's/\"//g')
+arch=$(go env GOARCH)
+
+go test || (echo 'Tests failed. Stopping...' && exit 1)
+
+for os in linux darwin; do
+  target="watchgopher-$version-$os-$arch"
+  echo "Building $target ..."
+
+  mkdir -p dist/$target
+  go build -o dist/$target/watchgopher .
+  cp README.md dist/$target
+
+  cd dist
+  tar czvf $target.tar.gz $target
+  cd -
+  rm -rf dist/$target
+done
